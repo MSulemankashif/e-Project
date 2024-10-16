61c0cd11c26a314ae1c1ccd318059402e210c3de
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-10-16 18:05:01 +0500

    cards

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// document.getElementById("homeIcon").addEventListener("click", function(e) {
+//     e.preventDefault(); // Prevent the default link behavior
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth" // This enables smooth scrolling
+//     });
+// });
