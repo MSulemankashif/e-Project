09f4557c8f16db38d916187c5f6ea2d31b9b10da
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-10-18 17:53:10 +0500

    cards

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
-// document.getElementById("homeIcon").addEventListener("click", function(e) {
-//     e.preventDefault(); // Prevent the default link behavior
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth" // This enables smooth scrolling
-//     });
-// });
+document.getElementById("homeIcon").addEventListener("click", function(e) {
+    e.preventDefault(); 
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}); // For scroll
+
 const searchIcon = document.querySelector('.fas.fa-search');
 const searchBar = document.querySelector('.search-bar input');
 
