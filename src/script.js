17beac28f3e788af11cb0681de61692246658f3a
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-10-21 16:30:34 +0500

    index

--- src/script.js
+++ src/script.js
@@ -12,20 +12,29 @@ const searchBar = document.querySelector('.search-bar input');
 searchIcon.addEventListener('click', () => {
   searchBar.focus();
 });
-const searchInput = document.querySelector('.search-bar input');
-const cards = document.querySelectorAll('.card');
 
-searchInput.addEventListener('input', () => {
-  const searchTerm = searchInput.value.toLowerCase();
-
-  cards.forEach(card => {
-    const title = card.querySelector('.title').textContent.toLowerCase();
-    const description = card.querySelector('.description').textContent.toLowerCase();
-
-    if (title.includes(searchTerm) || description.includes(searchTerm)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
+async function getSongs(){
+  let a = await fetch("http://127.0.0.1:5500/src/songs/")
+  let response = await a.text();
+  // console.log(response);
+  let div = document.createElement("div")
+  div.innerHTML= response;
+  let as=div.getElementsByTagName("a")
+  let songs = []
+  for (let index =0; index <as.length; index++){
+    const element = as[index];
+    if(element.href.endsWith(".mp3")){
+      songs.push(element.href)
     }
-  });
-});
+  }
+  return songs
+   
+}
+
+async function main (){
+  let songs = await getSongs()
+  console.log(songs);
+// var audio = new Audio(songs[0]);
+//   audio.play(); 
+}
+main()
